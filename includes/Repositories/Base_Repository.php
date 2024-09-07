102d330b9d7886405abf3cbd1e5034129ca218bf
James Allen <jacques2186@yahoo.com>
2024-09-07 14:57:18 -0400

    Update baseRepository to use Entity

--- includes/Repositories/Base_Repository.php
+++ includes/Repositories/Base_Repository.php
@@ -5,7 +5,7 @@ use Ohio_Tokyo_International_Sea_Monster_Society\Entities\Collection;
 
 interface Base_Repository
 {
-	public function create(array $data): bool;
+	public function create(array $data): ?Entity;
 	public function find(int $id): ?Entity;
 	public function findAll(?string $orderBy): Collection;
 }
